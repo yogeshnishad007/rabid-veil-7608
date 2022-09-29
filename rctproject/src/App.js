c53024abc870aec098c09b80c2a2345d1503e91e
yogeshnishad007 <yogiatoy27518@gmail.com>
2022-09-30 02:24:33 +0530

    Navbar

--- rctproject/src/App.js
+++ rctproject/src/App.js
@@ -1,26 +1,16 @@
-import Navbar from "./componant/Navbar"
-import Pricing from "./Pages/Pricing"
-import Home from "./Pages/Home"
-import About from "./Pages/About"
-import BackdropExample from "./componant/Modal"
-// import InitialFocus from "./componant/Modal"
-import { Route, Routes } from "react-router-dom"
+import "./App.css";
+
+import TopBar from "./componant/TopBar";
+
 
 function App() {
   return (
-    <>
-      <Navbar />
-      {/* <BackdropExample/> */}
-      {/* <InitialFocus/> */}
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </>
-  )
+    <div className="App">
+      <TopBar/>
+    
+   
+    </div>
+  );
 }
 
-export default App
+export default App;
